a9160f80b1cd593e90e82a6ae2f57aa6ea67b3dc
marie-kobayashi <marie.kobayashi.1016@gmail.com>
2018-11-04 19:59:20 +0900

    gulpfileにsass追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var less = require('gulp-less');
+var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
@@ -18,6 +19,13 @@ gulp.task('css', function(){
     .pipe(gulp.dest('build/css'))
 });
 
+// SassとCssの保存先を指定
+gulp.task('sass', function(){
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./css/'));
+});
+
 gulp.task('js', function(){
   return gulp.src('client/javascript/*.js')
     .pipe(sourcemaps.init())
@@ -26,4 +34,4 @@ gulp.task('js', function(){
     .pipe(gulp.dest('build/js'))
 });
 
-gulp.task('default', [ 'html', 'css', 'js' ]);
+gulp.task('default', [ 'html', 'css', 'sass', 'js']);
